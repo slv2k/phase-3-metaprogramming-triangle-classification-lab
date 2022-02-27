a1ec29f1550158de17105a824effc75656447ed4
samuel <samuelmmanor@gmail.com>
2022-02-26 22:48:59 -0500

    commit tree 10bb6cd0bca7d5692ccc8b8ce5da96f6010556f7

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,37 +1,39 @@
 class Triangle
-  # triangle code
-  attr_reader :a, :b, :c
+  attr_reader :side1, :side2, :side3
 
-  def initialize(a, b, c)
-    @a = a
-    @b = b
-    @c = c
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
   end
 
   def kind
     validate_triangle
-    if a == b && b == c
+    if side1 == side2 && side2 == side3
       :equilateral
-    elsif a == b || b == c || a == c
+    elsif side1 == side2 || side2 == side3 || side1 == side3
       :isosceles
     else
       :scalene
     end
   end
 
-  def sides_greater_than_zero?
-    [a, b, c].all?(&:positive?)
-  end
-
-  def valid_triangle_inequality?
-    a + b > c && a + c > b && b + c > a
-  end
-
-  def validate_triangle
+   def validate_triangle
     raise TriangleError unless sides_greater_than_zero? && valid_triangle_inequality?
   end
 
   class TriangleError < StandardError
     # triangle error code
   end
+
+  private
+
+  def sides_greater_than_zero?
+    [side1, side2, side3].all?(&:positive?)
+  end
+
+  def valid_triangle_inequality?
+    side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1
+  end
+
 end
